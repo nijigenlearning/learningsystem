b8795cdd04a9843a0e8183a8545477e6ab41fe23
nijigenlearning <sup20240101@gmail.com>
2025-07-25 14:32:55 +0900

    ログイン画面レイアウトのHTMLタグ重複エラーを修正

--- src/app/admin/login/layout.tsx
+++ src/app/admin/login/layout.tsx
@@ -6,12 +6,8 @@ interface LoginLayoutProps {
 
 export default function LoginLayout({ children }: LoginLayoutProps) {
   return (
-    <html lang="ja">
-      <body>
-        <div className="min-h-screen bg-gray-50">
-          {children}
-        </div>
-      </body>
-    </html>
+    <div className="min-h-screen bg-gray-50">
+      {children}
+    </div>
   );
 } 
